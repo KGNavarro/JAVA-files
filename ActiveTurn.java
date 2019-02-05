423ad1b874d7742a39bac41f1aa44c188bc70c75
Kevin Navarro <kgnavarro300@gmail.com>
2019-02-04 19:52:51 -0500

    Menu reorder, added conditional reactions
    Biggest change i made was changing the order of instantiation for the different menus since reactions and bonus actions can be conditional. To this effect i put the reactions and bonus actions first. I added the reaction button and reaction menu for keeping track of reactions taken. I used a menu instead of simply graying out the button since there are certain reactions you can take during your turn. Also added the use readied action as well as making it conditional if ready action was used. I added a reset turn button to reset the turn and the buttons to default states and divided the options menu and reset button into the left and right part of an HBox respectively.

--- ActiveTurn.java
+++ ActiveTurn.java
@@ -4,11 +4,13 @@ package c.o.a.s.t;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tooltip;
 import static javafx.scene.control.Tooltip.install;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
@@ -48,13 +50,137 @@ public class ActiveTurn extends Application {
         Option object = new Action();
         Option improvise = new Action();
         
+        Stage reactionStage = new Stage();//stage for the reactions menu
+        reactionStage.setWidth(300);
+        reactionStage.setHeight(300);
+        reactionStage.setResizable(false);
+        reactionStage.setTitle("Available reactions");
+        
+        Button reactionBtn = new Button();//main menu reaction button
+        reactionBtn.setText("Reaction");
+        reactionBtn.setOnAction(new EventHandler<ActionEvent>(){
+        
+            @Override
+            public void handle(ActionEvent event){
+                reactionStage.show();
+            }
+        });
+        
+        Button opportunityAttackBtn = new Button();
+        opportunityAttackBtn.setText("Opportunity Attack");
+        opportunityAttackBtn.setOnAction(new EventHandler<ActionEvent>(){
+        
+            @Override
+            public void handle(ActionEvent event){
+                reactionStage.hide();
+                reactionBtn.setDisable(true);
+            }
+        });
+        
+        Button improviseReactionBtn = new Button();
+        improviseReactionBtn.setText("Improvise reaction");
+        improviseReactionBtn.setOnAction(new EventHandler<ActionEvent>(){
+        
+            @Override
+            public void handle(ActionEvent event){
+                reactionStage.hide();
+                reactionBtn.setDisable(true);
+            }
+        });
+        
+        Button useReadiedActionBtn = new Button(); //set the default to false so that it can be available should the user use the ready action
+        useReadiedActionBtn.setText("Readied Action");
+        useReadiedActionBtn.setDisable(true);
+        useReadiedActionBtn.setOnAction(new EventHandler<ActionEvent>(){
+        
+            @Override
+            public void handle(ActionEvent event){
+                reactionStage.hide();
+                reactionBtn.setDisable(true);
+            }
+        });
+        
+        VBox reactionMenu = new VBox();
+        reactionMenu.setSpacing(2);
+        reactionMenu.getChildren().add(opportunityAttackBtn);
+        reactionMenu.getChildren().add(improviseReactionBtn);
+        reactionMenu.getChildren().add(useReadiedActionBtn);
+        
+        Scene reactionScene = new Scene(reactionMenu, 300, 300);
+        reactionStage.setScene(reactionScene);
+        
+        Stage bonusActionStage = new Stage();//window where you can see what acitons you can take this turn
+        bonusActionStage.setWidth(300);
+        bonusActionStage.setHeight(300);
+        bonusActionStage.setResizable(false);
+        bonusActionStage.setTitle("Available bonus actions");
+        
+        Button bonusActionBtn = new Button();
+        bonusActionBtn.setText("Bonus Actions");
+        bonusActionBtn.setOnAction(new EventHandler<ActionEvent>(){//see the available bonus actions
+        
+            @Override
+            public void handle(ActionEvent event){
+                bonusActionStage.show();
+            }
+        });
+        
+        Button unarmedStrikeBtn = new Button();
+        unarmedStrikeBtn.setText("Unarmed strike");
+        unarmedStrikeBtn.setOnAction(new EventHandler<ActionEvent>(){
+        
+            @Override
+            public void handle(ActionEvent event){
+                bonusActionStage.hide();
+                bonusActionBtn.setDisable(true);
+            }
+        });
+        Button flurryOfBlowsBtn = new Button();
+        flurryOfBlowsBtn.setText("Flurry of blows");
+        flurryOfBlowsBtn.setOnAction(new EventHandler<ActionEvent>(){
+        
+            @Override
+            public void handle(ActionEvent event){
+                bonusActionStage.hide();
+                bonusActionBtn.setDisable(true);
+            }
+        });
+        Button patientDefenseBtn = new Button();
+        patientDefenseBtn.setText("Patient defense");
+        patientDefenseBtn.setOnAction(new EventHandler<ActionEvent>(){
+        
+            @Override
+            public void handle(ActionEvent event){
+                bonusActionStage.hide();
+                bonusActionBtn.setDisable(true);
+            }
+        });
+        Button stepOfTheWindBtn = new Button();
+        stepOfTheWindBtn.setText("Step of the wind");
+        stepOfTheWindBtn.setOnAction(new EventHandler<ActionEvent>(){
+        
+            @Override
+            public void handle(ActionEvent event){
+                bonusActionStage.hide();
+                bonusActionBtn.setDisable(true);
+            }
+        });
+        
+        VBox bonusActionMenu = new VBox();
+        bonusActionMenu.setSpacing(1.5);
+        bonusActionMenu.getChildren().add(unarmedStrikeBtn);
+        bonusActionMenu.getChildren().add(flurryOfBlowsBtn);
+        bonusActionMenu.getChildren().add(patientDefenseBtn);
+        bonusActionMenu.getChildren().add(stepOfTheWindBtn);
+        
+        Scene bonusActionScene = new Scene(bonusActionMenu, 300, 300);
+        bonusActionStage.setScene(bonusActionScene);
+        
         Stage actionStage = new Stage();//window where you can see what acitons you can take this turn
         actionStage.setWidth(300);
         actionStage.setHeight(300);
         actionStage.setResizable(false);
         actionStage.setTitle("Available actions");
-        
-        
         
         Button actionBtn = new Button();
         actionBtn.setText("Actions");
@@ -134,6 +260,7 @@ public class ActiveTurn extends Application {
             public void handle(ActionEvent event){
                 actionStage.hide();
                 actionBtn.setDisable(true);
+                useReadiedActionBtn.setDisable(false);
             }
         });
         Button searchBtn = new Button();
@@ -166,7 +293,6 @@ public class ActiveTurn extends Application {
                 actionBtn.setDisable(true);
             }
         });
-                
         
         VBox actionMenu = new VBox();
         actionMenu.setSpacing(1.5);
@@ -184,75 +310,7 @@ public class ActiveTurn extends Application {
         Scene actionMenuScene = new Scene(actionMenu, 300, 300);
         actionStage.setScene(actionMenuScene);
         
-        
-        Stage bonusActionStage = new Stage();//window where you can see what acitons you can take this turn
-        bonusActionStage.setWidth(300);
-        bonusActionStage.setHeight(300);
-        bonusActionStage.setResizable(false);
-        bonusActionStage.setTitle("Available bonus actions");
-        
-        Button bonusActionBtn = new Button();
-        bonusActionBtn.setText("Bonus Actions");
-        bonusActionBtn.setOnAction(new EventHandler<ActionEvent>(){//see the available bonus actions
-        
-            @Override
-            public void handle(ActionEvent event){
-                bonusActionStage.show();
-            }
-        });
-        
-        Button unarmedStrikeBtn = new Button();
-        unarmedStrikeBtn.setText("Unarmed strike");
-        unarmedStrikeBtn.setOnAction(new EventHandler<ActionEvent>(){
-        
-            @Override
-            public void handle(ActionEvent event){
-                bonusActionStage.hide();
-                bonusActionBtn.setDisable(true);
-            }
-        });
-        Button flurryOfBlowsBtn = new Button();
-        flurryOfBlowsBtn.setText("Flurry of blows");
-        flurryOfBlowsBtn.setOnAction(new EventHandler<ActionEvent>(){
-        
-            @Override
-            public void handle(ActionEvent event){
-                bonusActionStage.hide();
-                bonusActionBtn.setDisable(true);
-            }
-        });
-        Button patientDefenseBtn = new Button();
-        patientDefenseBtn.setText("Patient defense");
-        patientDefenseBtn.setOnAction(new EventHandler<ActionEvent>(){
-        
-            @Override
-            public void handle(ActionEvent event){
-                bonusActionStage.hide();
-                bonusActionBtn.setDisable(true);
-            }
-        });
-        Button stepOfTheWindBtn = new Button();
-        stepOfTheWindBtn.setText("Step of the wind");
-        stepOfTheWindBtn.setOnAction(new EventHandler<ActionEvent>(){
-        
-            @Override
-            public void handle(ActionEvent event){
-                bonusActionStage.hide();
-                bonusActionBtn.setDisable(true);
-            }
-        });
-        
-        VBox bonusActionMenu = new VBox();
-        bonusActionMenu.setSpacing(1.5);
-        bonusActionMenu.getChildren().add(unarmedStrikeBtn);
-        bonusActionMenu.getChildren().add(flurryOfBlowsBtn);
-        bonusActionMenu.getChildren().add(patientDefenseBtn);
-        bonusActionMenu.getChildren().add(stepOfTheWindBtn);
-        
-        Scene bonusActionScene = new Scene(bonusActionMenu, 300, 300);
-        bonusActionStage.setScene(bonusActionScene);
-        
-        Button movementBtn = new Button();
+        Button movementBtn = new Button();    //might change this in the future to detail how many feet you can move, right now it seems a bit pedantic
         movementBtn.setText("Movement");
         movementBtn.setOnAction(new EventHandler<ActionEvent>(){
         
@@ -262,7 +320,7 @@ public class ActiveTurn extends Application {
             }
         });
         /**/
-        Button objectInteractionBtn = new Button();
+        Button objectInteractionBtn = new Button(); //made only to be gray out, items that require a use of object interaction will be put in the actions menu
         objectInteractionBtn.setText("Object Interaction");
         objectInteractionBtn.setOnAction(new EventHandler<ActionEvent>(){
         
@@ -273,10 +331,23 @@ public class ActiveTurn extends Application {
         });
         /**/
         
+        Button resetTurnBtn = new Button(); //resets everything to default states to start your turn over again
+        resetTurnBtn.setText("Reset your turn");
+        resetTurnBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                actionBtn.setDisable(false);
+                bonusActionBtn.setDisable(false);
+                objectInteractionBtn.setDisable(false);
+                movementBtn.setDisable(false);
+                reactionBtn.setDisable(false);
+                useReadiedActionBtn.setDisable(true);
+            }
+        });
         
-        Button btn = new Button();
-        btn.setText("What's my character?");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        Button characterBtn = new Button(); //just keeping this right now even though it only prints to console, will change to a display window with the char info
+        characterBtn.setText("What's my character?");
+        characterBtn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
             public void handle(ActionEvent event) {
@@ -284,15 +355,21 @@ public class ActiveTurn extends Application {
             }
         });
         
-        VBox root = new VBox();
-        root.getChildren().add(btn);
-        root.getChildren().add(actionBtn);
-        root.getChildren().add(bonusActionBtn);
-        root.getChildren().add(movementBtn);
-        root.getChildren().add(objectInteractionBtn);
+        HBox root = new HBox();
+        VBox mainMenu = new VBox();
+        mainMenu.getChildren().add(characterBtn);
+        mainMenu.getChildren().add(actionBtn);
+        mainMenu.getChildren().add(bonusActionBtn);
+        mainMenu.getChildren().add(movementBtn);
+        mainMenu.getChildren().add(objectInteractionBtn);
+        mainMenu.getChildren().add(reactionBtn);
         
+        root.getChildren().add(mainMenu);
+        root.getChildren().add(resetTurnBtn);
+        root.spacingProperty().bind(root.widthProperty().divide(4));
+                
         Scene scene = new Scene(root, 300, 250);
-        
+                
         primaryStage.setTitle("C.O.A.S.T.");
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
